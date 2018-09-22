ad6c6826f0a7eeb2254398b0ed4476f4ea6dedb3
gxagxagxa <gxagxagxa@me.com>
2018-09-22 23:55:21 +0800

    add test for unicode

--- tests/test_input.py
+++ tests/test_input.py
@@ -17,3 +17,11 @@ class TestInput(TestCase):
         self.assertEqual(input.trim_out, None)
         self.assertEqual(input.fps, None)
         self.assertEqual(input.sequence, False)
+        input = Input(u'某些中文 和的 tv.mp4', start=1001, trim_in_frame=10,
+                      trim_out_frame=40, fps=24.0)
+        self.assertEqual(input.filename, u'某些中文 和的 tv.mp4')
+        self.assertEqual(input.start, 1001)
+        self.assertEqual(input.trim_in, 10)
+        self.assertEqual(input.trim_out, 40)
+        self.assertEqual(input.fps, 24.0)
+        self.assertEqual(input.sequence, False)
