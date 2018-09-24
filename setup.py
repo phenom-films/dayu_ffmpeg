e2feb34be2176c54b7c80200cf2947bb1815c05f
gxagxagxa <gxagxagxa@me.com>
2018-09-24 19:50:29 +0800

    init setup for pypi

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+__author__ = 'andyguo'
+
+from setuptools import setup
+
+setup(
+        name='dayu_ffmpeg',
+        version='0.2',
+        packages=['dayu_ffmpeg'],
+        url='https://github.com/phenom-films/dayu_ffmpeg',
+        license='MIT',
+        author='andyguo',
+        author_email='andyguo@phenom-films.com',
+        description='FFmpeg python wrapper with common filter built in.'
+)
