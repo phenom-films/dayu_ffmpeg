b169b9cac19f4e55f5f918666aee45108549432d
gxagxagxa <gxagxagxa@me.com>
2018-09-24 20:05:21 +0800

    update setup

--- setup.py
+++ setup.py
@@ -7,11 +7,19 @@ from setuptools import setup
 
 setup(
         name='dayu_ffmpeg',
-        version='0.2',
+        version='0.3',
         packages=['dayu_ffmpeg'],
         url='https://github.com/phenom-films/dayu_ffmpeg',
         license='MIT',
         author='andyguo',
         author_email='andyguo@phenom-films.com',
-        description='FFmpeg python wrapper with common filter built in.'
+        description='FFmpeg python wrapper with common filter built in.',
+        long_description=open('README.rst').read(),
+        classifiers=[
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: Implementation',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+        ],
 )
