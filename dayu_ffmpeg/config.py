0baf61d5db408b8314d89e17795b457066ed3e1a
gxagxagxa <gxagxagxa@me.com>
2018-09-24 18:40:02 +0800

    linux has built-in ffmpeg

--- dayu_ffmpeg/config.py
+++ dayu_ffmpeg/config.py
@@ -8,7 +8,7 @@ import os
 # 记录三个平台下，ffmpeg 的执行文件路径
 FFMPEG_EXEC = {'win32' : os.sep.join((os.path.dirname(__file__), 'static', 'bin', 'win32', 'ffmpeg.exe')),
                'darwin': os.sep.join((os.path.dirname(__file__), 'static', 'bin', 'darwin', 'ffmpeg')),
-               'linux2': 'ffmpeg'}
+               'linux2': os.sep.join((os.path.dirname(__file__), 'static', 'bin', 'linux2', 'ffmpeg'))}
 
 FFMPEG_DEFAULT_FONT = {
     'win32' : os.sep.join((os.path.dirname(__file__), 'static', 'font', 'SourceHanSansK-Regular.ttf')),
