54560d95d419e250fc0c6bad2323d39ff63f4c50
Andy Guo <andyguo@phenom-films.com>
2018-09-25 11:42:29 +0800

    update version to 0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
         name='dayu_ffmpeg',
-        version='0.3',
+        version='0.4',
         packages=['dayu_ffmpeg'],
         url='https://github.com/phenom-films/dayu_ffmpeg',
         license='MIT',
