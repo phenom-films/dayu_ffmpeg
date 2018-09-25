b18f76e62cdc1291de8a3ed0b90d8f1125c4f391
Andy Guo <andyguo@phenom-films.com>
2018-09-25 11:15:24 +0800

    add test for srt subtitle

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -22,6 +22,22 @@ def test_mov_stream():
     os.remove(output_mov)
 
 
+def test_draw_subtitle_stream():
+    input_mov = os.sep.join((os.path.dirname(__file__), 'footage', 'media', '10s.mov'))
+    output_mov = os.sep.join((os.path.dirname(__file__), 'footage', 'media', 'output.mov'))
+    command = FFmpeg() >> Overwrite() >> \
+              Input(input_mov) >> DrawMask(2.39) >> Scale(width=1280, height=720) >> \
+              DrawSubtitle([u'中文', '1', '2'], use_python_list=True) >> \
+              DrawTimecode() >> DrawDate() >> DrawFrames() >> \
+              Codec() >> Output(output_mov)
+
+    for x in command.run():
+        print x
+
+    assert os.path.exists(output_mov)
+    os.remove(output_mov)
+
+
 def test_seq_stream():
     input_mov = os.sep.join((os.path.dirname(__file__), 'footage', 'seq', 'transcode_%04d.jpg'))
     output_mov = os.sep.join((os.path.dirname(__file__), 'footage', 'media', 'output.mov'))
