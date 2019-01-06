d9f189c5f649c68d5038d15851abbb4c974960e5
gxagxagxa <gxagxagxa@me.com>
2019-01-06 21:08:23 +0800

    implement holder to_script and from_script

--- dayu_ffmpeg/network/node/holder.py
+++ dayu_ffmpeg/network/node/holder.py
@@ -27,7 +27,13 @@ class InputHolder(AbstractHolder):
         self.parent = kwargs.get('parent', None)
         self.out_edges = [UniqueList() for _ in range(self.max_output_num)]
         self.link_num = None
+        self.knobs = UniqueList()
         self.__dict__.update(kwargs)
+
+    def validate(self):
+        if self.input(0):
+            return True
+        return False
 
     @property
     def in_edges(self):
@@ -70,6 +76,32 @@ class InputHolder(AbstractHolder):
         node = self.input(0)
         return node.origin() if node else None
 
+    def to_script(self):
+        from copy import deepcopy
+        result = deepcopy(self.__dict__)
+        result['id'] = self.id
+        result['type'] = self.type
+        result['name'] = self.name
+        result['label'] = self.label
+        result['metadata'] = self.metadata
+        result['pos_x'] = self.pos_x
+        result['pos_y'] = self.pos_y
+        result['selected'] = self.selected
+        result['parent'] = self.parent.id if self.parent else None
+        result['out_edges'] = [g.to_script() for g in self.out_edges]
+        result['knobs'] = [k.to_script() for k in self.knobs]
+        return result
+
+    @classmethod
+    def from_script(cls, object):
+        from dayu_ffmpeg.ffscript import parse_ffscript_data
+        instance = cls()
+        instance.__dict__.update(object)
+        instance.parent = parse_ffscript_data(instance.parent)
+        instance.out_edges = [UniqueList([parse_ffscript_data(e) for e in g]) for g in instance.out_edges]
+        instance.knobs = UniqueList([parse_ffscript_data(k) for k in instance.knobs])
+        return instance
+
 
 class OutputHolder(AbstractHolder):
     type = 'output_holder'
@@ -87,6 +119,7 @@ class OutputHolder(AbstractHolder):
         self.parent = kwargs.get('parent', None)
         self.in_edges = UniqueList([None for _ in range(self.max_input_num)])
         self.link_num = None
+        self.knobs = UniqueList()
         self.__dict__.update(kwargs)
 
     @property
@@ -122,3 +155,29 @@ class OutputHolder(AbstractHolder):
     def origin(self):
         node = self.input(0)
         return node.origin() if node else None
+
+    def to_script(self):
+        from copy import deepcopy
+        result = deepcopy(self.__dict__)
+        result['id'] = self.id
+        result['type'] = self.type
+        result['name'] = self.name
+        result['label'] = self.label
+        result['metadata'] = self.metadata
+        result['pos_x'] = self.pos_x
+        result['pos_y'] = self.pos_y
+        result['selected'] = self.selected
+        result['parent'] = self.parent.id if self.parent else None
+        result['in_edges'] = [e.to_script() if e else None for e in self.in_edges]
+        result['knobs'] = [k.to_script() for k in self.knobs]
+        return result
+
+    @classmethod
+    def from_script(cls, object):
+        from dayu_ffmpeg.ffscript import parse_ffscript_data
+        instance = cls()
+        instance.__dict__.update(object)
+        instance.parent = parse_ffscript_data(instance.parent)
+        instance.in_edges = UniqueList([parse_ffscript_data(e) for e in instance.in_edges])
+        instance.knobs = UniqueList([parse_ffscript_data(k) for k in instance.knobs])
+        return instance
