fb9a55b53a27d75ccd80ecbecbaae01f088232cf
Andy Guo <andyguo@phenom-films.com>
2019-01-08 12:49:26 +0800

    udpate setup to version 0.5

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ from setuptools import setup
 
 setup(
         name='dayu_ffmpeg',
-        version='0.4',
+        version='0.5',
         packages=['dayu_ffmpeg'],
         url='https://github.com/phenom-films/dayu_ffmpeg',
         license='MIT',
         author='andyguo',
         author_email='andyguo@phenom-films.com',
-        description='FFmpeg python wrapper with common filter built in.',
+        description='FFmpeg python wrapper for human, with common filters built in.',
         long_description=open('README.rst').read(),
         classifiers=[
             'Operating System :: OS Independent',
