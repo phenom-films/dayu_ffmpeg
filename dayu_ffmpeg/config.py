45ebea5f4118bd788c8e4a3cd56df1afd485380f
gxagxagxa <gxagxagxa@me.com>
2019-01-09 23:53:08 +0800

    add order score constant

--- dayu_ffmpeg/config.py
+++ dayu_ffmpeg/config.py
@@ -16,3 +16,12 @@ FFMPEG_DEFAULT_FONT = {
     'linux2': os.sep.join((os.path.dirname(__file__), 'static', 'font', 'SourceHanSansK-Regular.ttf'))}
 
 SINGLE_MEDIA_FORMAT = (u'.mov', u'.mp4', u'.mkv')
+
+GLOBAL_ORDER_SCORE = 10
+INPUT_ORDER_SCORE = 20
+INPUT_HOLDER_ORDER_SCORE = 21
+FILTER_ORDER_SCORE = 30
+CODEC_ORDER_SCORE = 40
+OUTPUT_HOLDER_ORDER_SCORE = 99
+OUTPUT_ORDER_SCORE = 100
+GROUP_ORDER_SCORE = 0
