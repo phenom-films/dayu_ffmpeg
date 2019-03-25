cc7a83234c01b5526cc57c715e4b8df61b08051a
Andy Guo <andyguo@phenom-films.com>
2019-03-25 12:06:12 +0800

    fix pip setup packages

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ from setuptools import setup
 setup(
         name='dayu_ffmpeg',
         version='0.5.1',
-        packages=['dayu_ffmpeg'],
+        packages=['dayu_ffmpeg', 'dayu_ffmpeg.network', 'dayu_ffmpeg.errors', 'dayu_ffmpeg.network.edge',
+                  'dayu_ffmpeg.network.knob', 'dayu_ffmpeg.network.node'],
+        include_package_data=True,
         url='https://github.com/phenom-films/dayu_ffmpeg',
         license='MIT',
         author='andyguo',
