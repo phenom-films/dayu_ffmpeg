602c757f8b3b527bdb9bb0bfa4f3d4d9885572c5
yangzhuo <894647985@qq.com>
2019-11-20 17:45:51 +0800

    commit custom template slate

--- dayu_ffmpeg/util.py
+++ dayu_ffmpeg/util.py
@@ -6,3 +6,7 @@ __author__ = 'andyguo'
 
 def get_safe_string(string):
     return u'{}'.format(string.replace('\\', '\\\\').replace(':', '\\:').replace('%', r'\\\\%'))
+
+
+print get_safe_string('%Y-%m-%d %H:%M:%S')
+
